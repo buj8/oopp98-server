b3d9431018f53a413005b05118ad07912f69fa85
buj8 <alvarobuj8@gmail.com>
2019-04-07 14:37:06 +0200

    Updated classes  (DbAdaptor and PointsController)

--- src/main/java/utility/DbAdaptor.java
+++ src/main/java/utility/DbAdaptor.java
@@ -635,15 +635,15 @@ public class DbAdaptor {
      * @param username the current user
      * @return count of pending friend requests
      */
-    public int retrieveCount(String username){
+    public int retrieveCount(String username) {
         connect();
         int result = -1;
-        String sql = "select count(from_user) from friend_request where (from_user = ? or to_user = ?) and friend_status = ?::\"friend_status\"";
+        String sql = "select count(from_user) from friend_request "
+                + "where to_user = ? and friend_status = ?::\"friend_status\"";
         try {
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(1,username);
-            statement.setString(2,username);
-            statement.setString(3, FriendStatus.PENDING.name());
+            statement.setString(2, FriendStatus.PENDING.name());
             rs = statement.executeQuery();
             if (rs.next()) {
                 result = rs.getInt(1);
